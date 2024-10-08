217666c776ce7cce7f16ed9a134b8f4fe72ec210
maxi <maxinunez2455@gmail.com>
2024-10-08 00:16:50 -0300

    ejercicio 14

---
+++ tp3/ej14.sh
@@ -0,0 +1,25 @@
+#!/Segubin/bash
+if [ $# -lt 3 ]; then
+	echo "Por favor ingrese un directorio, -a o -b y una cadena"
+else
+	if [ -d $1 ]; then
+		case $2 in 
+			-a) 
+				for arc in $(ls "$1"); do 
+					mv "$1/$arc" "$1/${arc}$3"
+				done
+			;;
+			-b)
+				for arc in $(ls "$1"); do
+					mv "$1/$arc" "$1/${3}$arc"
+				done
+			;;
+			*)
+				echo "Por favor ingrese un parámetro válido (-a o -b)"
+			;;
+		esac
+	else
+		echo "$1 No es un directorio existente"
+	fi
+fi				
+		
